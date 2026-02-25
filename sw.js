88eae965833330e30466158022bc76400b574525
adityakumar34053 <mradityakumar669@gmail.com>
2026-02-25 06:38:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
 
+// Install Event
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Family App Install');
+});
+
+// Fetch Event - Ye lagana sabse zaroori hai tabhi "Install" ka popup aayega!
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            console.log("Network request failed");
+        })
+    );
+});
